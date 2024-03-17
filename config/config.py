a5b186c73cd0cec4190faec2520c49e0e0f96fc2
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-17 22:00:38 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -43,7 +43,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TheTeamAlexa/AlexaMusic",
+    "https://github.com/qwert200/as",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
